1dac99c25f0ff77eccc268fb2bdc135c3092f033
username <email>
2020-12-12 09:12:29 +0800

    添加品牌

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,15 @@
-export {default as login} from './acl/login' // {default: {}}
-export {default as user} from './acl/user'
-export {default as role} from './acl/role'
-export {default as permission} from './acl/permission'
-export {default as category} from './category'
-export {default as clientUser} from './clientUser'
-export {default as order} from './order'
+export { default as login } from "./acl/login"; // {default: {}}
+export { default as user } from "./acl/user";
+export { default as role } from "./acl/role";
+export { default as permission } from "./acl/permission";
+export { default as category } from "./category";
+export { default as clientUser } from "./clientUser";
+// 1. 引入默认暴露的内容重命名为order
+// 2. 将order给分别暴露出去
+export { default as order } from "./order";
+export { default as trademark } from "./product/trademark";
+
+// import { default as order } from './order' // 完整引入
+// import order from './order' // 完整引入的简写
+// export order;
+
