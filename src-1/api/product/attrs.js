1ebe6d7ccd8543610732215d61156dbb4a05ee88
username <email>
2020-12-13 21:22:32 +0800

    辛辣天塞

---
+++ src-1/api/product/attrs.js
@@ -0,0 +1,68 @@
+import request from "@utils/request";
+
+const api_name = "/admin/product";
+
+export default {
+  // 获取一级分类列表
+
+  getCategorys1() {
+    return request({
+      url: `${api_name}/getCategory1`,
+      method: "GET"
+    });
+  },
+  // 获取2级分类列表
+  getCategorys2(category1Id) {
+    return request({
+      url: `${api_name}/getCategory2/${category1Id}`,
+      method: "GET"
+    });
+  },
+  // 获取3级分类列表
+  getCategorys3(category1Id) {
+    return request({
+      url: `${api_name}/getCategory3/${category2Id}`,
+      method: "GET"
+    });
+  },
+  // 获取分类对应的属性列表
+  getAttrList({
+    category1Id,
+    category2Id,
+    category3Id
+  }) {
+    return request({
+      url: `${api_name}/getCategory3/${category2Id}/${category3Id}`,
+      method: "GET",
+    });
+  },
+  // 删除属性
+  deleteAttr(attrId) {
+    return request({
+      url: `${api_name}/deleteAttr/${attrId}`,
+      method: "DELETE",
+    });
+  },
+  // 获取属性的属性值列表
+  getAttrValueList(attrId) {
+    return request({
+      url: `${api_name}/getAttrValueList/${attrId}`,
+      method: "GET"
+    });
+  },
+  // 获取属性的属性值列表
+  getAttrList(attrId) {
+    return request({
+      url: `${api_name}/getAttrValueList/${attrId}`,
+      method: "GET"
+    });
+  },
+  // 保存属性
+  saveAttrInfo(attr) {
+    return request({
+      url: `${api_name}/saveAttrInfo`,
+      method: "POST",
+      data: attr
+    });
+  }
+};
--- src/api/index.js
+++ src/api/index.js
@@ -8,8 +8,22 @@ export { default as clientUser } from "./clientUser";
 // 2. 将order给分别暴露出去
 export { default as order } from "./order";
 export { default as trademark } from "./product/trademark";
+export { default as attrs } from "./product/attrs";
 
 // import { default as order } from './order' // 完整引入
 // import order from './order' // 完整引入的简写
 // export order;
 
+/*
+  {
+    login: { login() {}... },
+    user,
+    role,
+    permission,
+    category,
+    clientUser,
+    order,
+    trademark,
+    attrs
+  }
+*/
